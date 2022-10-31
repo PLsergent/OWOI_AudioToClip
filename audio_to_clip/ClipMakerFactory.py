a2ac863318ac0d784aa2f355bb221f913d164497
Pierre-Louis Sergent <papa.louis59@gmail.com>
2022-10-31 12:04:19 +0100

    Minor adv

--- audio_to_clip/ClipMakerFactory.py
+++ audio_to_clip/ClipMakerFactory.py
@@ -7,10 +7,17 @@ from audio_to_clip.WordTimestamp import WordTimestamp
 
 @dataclass
 class ClipMakerFactory:
+    video_name: str
+    username: str
     words_timestamps: list[WordTimestamp]
+    audio_file_clip: AudioFileClip = None
+    video_file_clip: VideoFileClip = None
+    gcs_storage_uri: str = f"gs://videos/{username}/{video_name}"
     google_credentials_key: str = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
     def get_audio_file(self, filename: str) -> AudioFileClip:
+        if self.audio_file_clip is not None:
+            return self.audio_file_clip
         try:
             os.makedirs("./tmp/", exist_ok=True)
             bucket = self.storage_client.bucket(self.gcs_uri)
@@ -20,3 +27,7 @@ class ClipMakerFactory:
         except:
             traceback.print_exc()
             raise Exception("Could not download audio file from GCS")
+    
+    def get_video_file(self) -> VideoFileClip:
+        if self.video_file_clip is not None:
+            return self.video_file_clip
