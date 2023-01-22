ac3c16708f04f507ee21b99273f2347ad9917f54
Pierre-Louis Sergent <papa.louis59@gmail.com>
2023-01-22 15:52:05 +0100

    Adding leo's work + documentation, clip_maker done

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # OWOI_AudioToClip
 Python module used for the school project OWOI (One Word One Image)
+
+## Installation
+
+After git cloning the repository, you can install the dependencies with the following command:
+
+```bash
+poetry install
+```
+
+## Credentials
+
+Please provide your credentials in the following environment variables:
+
+```bash
+export GOOGLE_APPLICATION_CREDENTIALS="path/to/credentials.json"
+export GOOGLE_IMAGES_SEARCH_TOKEN="token"
+export GOOGLE_SEARCH_ID="id"
+```
+
+## Classes
+
+### TranscriptFactory
+
+This class is used to create a transcript from a text file. It will create a list of words and a list of timestamps.
+
+```python	
+from audio_to_clip import ClipMakerFactory
+
+transcript_factory = TranscriptFactory(gcs_uri="gs://bucket/file.mp3")
+```
+
+Methods:
+- ***transcribe_audio_to_text() -> list[dict]***: transcribe audio to text from the gcs_uri and returns a list of dict with the following keys: "word", "start_time" and "end_time"
+- ***get_word_timestamps() -> list[dict]***: returns a list of dict with the following keys: "word", "start_time" and "end_time"
+
+This Class should be used to create a transcript from a text file before creating a clip with the ClipMakerFactory.
+
+### ClipMakerFactory
+
+This class is used to create a clip from a transcript.
+
+```python
+from audio_to_clip import ClipMakerFactory
+
+clip_makfer_factory = ClipMagerFactory(video_name, username, transcript, gcs_bucket_dest, local_storage_dest, gcs_bucket_audio)
+```
+
+Params:
+- ***video_name***:str -> name of the video
+- ***username***:str -> name of the user
+- ***transcript***:list[WordTimestamp] -> list of WordTimestamp
+- ***gcs_bucket_dest***:str -> name of the gcs bucket destination
+- ***local_storage_dest***:str -> path to the local storage destination
+- ***gcs_bucket_audio***:str -> name of the gcs bucket where the audio is stored
+
+Methods:
+- ***clip_maker(word_timestamps: list[WordTimestamp]) -> VideoFileClip***: creates a clip from the transcript and returns a VideoFileClip
+- ***upload_video_to_gcs() -> None***: uploads the video to the gcs bucket with the name: `username/video_name.mp4`, should be called after clip_maker
--- audio_to_clip/ClipMakerFactory.py
+++ audio_to_clip/ClipMakerFactory.py
@@ -1,28 +1,36 @@
 from dataclasses import dataclass
 import os
 import traceback
-from moviepy.editor import AudioFileClip, VideoFileClip
+import requests
+from moviepy.editor import AudioFileClip, VideoFileClip, ImageClip
 from google.cloud import storage
+from google_images_search import GoogleImagesSearch
 
 from audio_to_clip.WordTimestamp import WordTimestamp
+
 
 @dataclass
 class ClipMakerFactory:
     video_name: str
     username: str
     words_timestamps: list[WordTimestamp]
+    gcs_bucket_dest: str
+    local_dest: str
+    gcs_bucket_audio: str
     audio_file_clip: AudioFileClip = None
     video_file_clip: VideoFileClip = None
+    images: list[str] = []
     storage_client = storage.Client()
-    gcs_storage_uri: str = f"gs://videos/{username}/{video_name}"
+    google_images_search_token = os.environ.get("GOOGLE_IMAGES_SEARCH_TOKEN")
+    google_search_id = os.environ.get("GOOGLE_SEARCH_ID")
     google_credentials_key: str = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
-    def get_audio_file(self, filename: str) -> AudioFileClip:
+    def _get_audio_file(self, filename: str) -> AudioFileClip:
         if self.audio_file_clip is not None:
             return self.audio_file_clip
         try:
             os.makedirs("./tmp/", exist_ok=True)
-            bucket = self.storage_client.bucket(self.gcs_uri)
+            bucket = self.storage_client.bucket(self.gcs_bucket_audio)
             blob = bucket.blob(filename)
             blob.download_to_filename("./tmp/" + filename)
             return AudioFileClip("./tmp/" + filename)
@@ -30,6 +38,49 @@ class ClipMakerFactory:
             traceback.print_exc()
             raise Exception("Could not download audio file from GCS")
     
+    def clip_maker(self, word_timestamps: list[WordTimestamp]) -> VideoFileClip:
+        try:
+            for wordts in word_timestamps:
+                self.images.append(wordts.get_word_dict())
+            
+            clips = [self._define_image_clip(img_dict) for img_dict in self.images]
+            self.video_file_clip = VideoFileClip.concatenate_videoclips(clips)
+            self.video_file_clip.write_videofile(
+                self.local_dest + self.video_name + ".mp4", fps=24, codec="mpeg4"
+            )
+            self.video_file_clip.set_audio(self._get_audio_file(self.video_name + ".mp3"))
+            return self.video_file_clip
+        except:
+            traceback.print_exc()
+            raise Exception("Could not create video file clip")
+    
+    def _get_image_url_from_google_image_search(self, word: str) -> str:
+        gis = GoogleImagesSearch(self.google_images_search_token, self.google_search_id)
+        _search_params = {
+            'q': word,
+            'num': 1,
+            'safe': 'off',
+            'fileType': 'png',
+            'imgType': 'photo',
+            'imgSize': 'MEDIUM',
+            'rights': 'cc_publicdomain'
+        }
+        gis.search(search_params=_search_params)
+        return gis.results()[0].url
+    
+    def _define_image_clip(self, img_dict: dict) -> ImageClip:
+        url = self._get_image_url_from_google_image_search(img_dict['word'])
+        response = requests.get(url)
+        open(self.local_dest + img_dict['word'] + '.png', 'wb').write(response.content)
+        return ImageClip(
+                    self.local_dest + img_dict['word'] + '.png'
+                ).set_duration(img_dict['end_time'] - img_dict['start_time'])
+    
+    def upload_video_to_gcs(self):
+        bucket = self.storage_client.bucket(self.gcs_bucket_dest)
+        blob = bucket.blob(f"{self.username}/{self.video_name}.mp4")
+        blob.upload_from_filename(f"{self.local_dest}{self.username}/{self.video_name}.mp4")
+
     def get_video_file(self) -> VideoFileClip:
         if self.video_file_clip is not None:
             return self.video_file_clip
