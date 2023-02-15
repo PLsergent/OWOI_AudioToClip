9c2b3c299ba2fc3dd692eef85489bae4fc91d266
Pierre-Louis Sergent <papa.louis59@gmail.com>
2023-02-16 00:38:05 +0100

    V1 done

--- samples/basic_example_usage.py
+++ samples/basic_example_usage.py
@@ -4,12 +4,12 @@ from owoi_audio_to_clip.TranscriptFactory import TranscriptFactory
 from owoi_audio_to_clip.ClipMakerFactory import ClipMakerFactory
 
 
-transcript_factory_test = TranscriptFactory("gs://owoi_bucket/test_audio/tell_me_which_one.wav")
+transcript_factory_test = TranscriptFactory("gs://owoi_bucket/test_audio/rap_god.wav")
 
 transcript = transcript_factory_test.transcribe_audio_to_text()
 
 print(transcript)
 
-clip_maker_factory = ClipMakerFactory("test_video", "username", transcript, "owoi_bucket", "../tmp/", "test_audio/tell_me_which_one.wav")
+clip_maker_factory = ClipMakerFactory("rap_god", "username", transcript, "owoi_bucket", "../tmp/", "test_audio/rap_god.wav")
 
 clip = clip_maker_factory.clip_maker()
