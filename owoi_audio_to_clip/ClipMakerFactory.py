a4d138c4f0cd5a6b92bbe949c329fffed116a2f1
Pierre-Louis Sergent <papa.louis59@gmail.com>
2023-02-18 19:36:48 +0100

    Add subtitles

--- owoi_audio_to_clip/ClipMakerFactory.py
+++ owoi_audio_to_clip/ClipMakerFactory.py
@@ -2,7 +2,17 @@ from dataclasses import dataclass
 import os
 import traceback
 import requests
-from moviepy.editor import AudioFileClip, VideoFileClip, ImageClip, ColorClip, concatenate_videoclips
+from moviepy.editor import (
+    AudioFileClip,
+    VideoFileClip,
+    ImageClip,
+    ColorClip,
+    TextClip,
+    CompositeVideoClip,
+    concatenate_videoclips,
+)
+from moviepy.video.tools.subtitles import SubtitlesClip
+
 from google.cloud import storage
 from google_images_search import GoogleImagesSearch
 
@@ -31,65 +41,106 @@ class ClipMakerFactory:
             os.makedirs(f"{self.local_dest}/{self.username}/audios", exist_ok=True)
             bucket = self.storage_client.bucket(self.gcs_bucket)
             blob = bucket.blob(f"{self.username}/audios/{audio_name}.wav")
-            blob.download_to_filename(f"{self.local_dest}/{self.username}/audios/{audio_name}.wav")
-            return AudioFileClip(f"{self.local_dest}/{self.username}/audios/{audio_name}.wav")
+            blob.download_to_filename(
+                f"{self.local_dest}/{self.username}/audios/{audio_name}.wav"
+            )
+            return AudioFileClip(
+                f"{self.local_dest}/{self.username}/audios/{audio_name}.wav"
+            )
         except:
             traceback.print_exc()
             raise Exception("Could not download audio file from GCS")
-    
+
     def clip_maker(self) -> VideoFileClip:
         try:
-            clips = [self._define_image_clip(wordts.get_word_dict()) for wordts in self.words_timestamps]
+            clips = [
+                self._define_image_clip(wordts.get_word_dict())
+                for wordts in self.words_timestamps
+            ]
             self.video_file_clip = concatenate_videoclips(clips, method="compose")
-            self.video_file_clip = self.video_file_clip.set_audio(self._get_audio_file(self.gcs_audio_name))
+            self.video_file_clip = self.video_file_clip.set_audio(
+                self._get_audio_file(self.gcs_audio_name)
+            )
             os.makedirs(f"{self.local_dest}/{self.username}/videos", exist_ok=True)
+            self.video_file_clip = CompositeVideoClip(
+                [self.video_file_clip, self._generate_subtitles().set_pos(("center", "bottom"))]
+            )
             self.video_file_clip.write_videofile(
-                f"{self.local_dest}/{self.username}/videos/{self.video_name}.mp4", fps=24, codec="mpeg4", bitrate="5000k"
+                f"{self.local_dest}/{self.username}/videos/{self.video_name}.mp4",
+                fps=24,
+                codec="mpeg4",
+                bitrate="5000k",
             )
             return self.video_file_clip
         except:
             traceback.print_exc()
             raise Exception("Could not create video file clip")
-    
-    def _get_image_url_from_google_image_search(self, word: str, num: int = 1) -> str:
+
+    def _generate_subtitles(self) -> SubtitlesClip:
+        generator = lambda txt: TextClip(txt, bg_color="black", stroke_width=3, font="Arial", fontsize=72, color="yellow")
+        subs = []
+        for wordts in self.words_timestamps:
+            word = wordts.get_word_dict()
+            if word["word"] != "###":
+                subs.append(
+                    (
+                        (word["start"].total_seconds(), word["end"].total_seconds()),
+                        word["word"],
+                    )
+                )
+        return SubtitlesClip(subs, generator).margin(bottom=40)
+
+    def _get_image_url_from_google_image_search(self, word: str, num: int = 1, img_size: str = "xlarge") -> str:
         gis = GoogleImagesSearch(self.google_images_search_token, self.google_search_id)
         _search_params = {
-            'q': word,
-            'num': num,
-            'imgSize': 'xlarge',
-            'safe': 'medium',
-            'imgColorType': 'color'
+            "q": word,
+            "num": num,
+            "imgSize": img_size,
+            "safe": "medium",
+            "imgColorType": "color",
         }
         gis.search(search_params=_search_params)
         if len(gis.results()) == 0:
             return "https://img.freepik.com/premium-vector/website-page-found-error-robot-character-with-magnifying-glass-hand-site-crash-technical_502272-1890.jpg?w=2000"
-        return gis.results()[num-1].url
-    
+        return gis.results()[num - 1].url
+
     def _define_image_clip(self, wordts: dict) -> ImageClip:
-        if wordts['word'] == "###":
-            color_clip = ColorClip((1920, 1080), (0,0,0))
+        if wordts["word"] == "###":
+            color_clip = ColorClip((1920, 1080), (0, 0, 0))
             if wordts["start"] == 0:
-                color_clip = color_clip.set_duration(wordts['end'].total_seconds())
+                color_clip = color_clip.set_duration(wordts["end"].total_seconds())
             else:
-                color_clip = color_clip.set_duration(wordts['end'].total_seconds() - wordts['start'].total_seconds())
+                color_clip = color_clip.set_duration(
+                    wordts["end"].total_seconds() - wordts["start"].total_seconds()
+                )
             return color_clip
 
         num = 1
         image_clip = self._get_image_clip(wordts, num)
         while image_clip is None:
+            if num == 100:
+                image_clip = ImageClip(
+                    "https://img.freepik.com/premium-vector/website-page-found-error-robot-character-with-magnifying-glass-hand-site-crash-technical_502272-1890.jpg?w=2000"
+                )
+                break
             num += 1
-            image_clip = self._get_image_clip(wordts, num)
-        return image_clip.set_duration(wordts['end'].total_seconds() - wordts['start'].total_seconds())
-        
+            img_size = "large"
+            image_clip = self._get_image_clip(wordts, num, img_size)
+        return image_clip.set_duration(
+            wordts["end"].total_seconds() - wordts["start"].total_seconds()
+        )
+
     def _get_image_clip(self, wordts: dict, num: int):
         try:
             for file in os.listdir(self.local_dest):
-                if file == wordts['word'] + '.jpg':
-                    return ImageClip(self.local_dest + wordts['word'] + '.jpg')
-            url = self._get_image_url_from_google_image_search(wordts['word'], num=num)
+                if file == wordts["word"] + ".jpg":
+                    return ImageClip(self.local_dest + wordts["word"] + ".jpg")
+            url = self._get_image_url_from_google_image_search(wordts["word"], num=num)
             response = requests.get(url)
-            open(self.local_dest + wordts['word'] + '.jpg', 'wb').write(response.content)
-            image_clip = ImageClip(self.local_dest + wordts['word'] + '.jpg')
+            open(self.local_dest + wordts["word"] + ".jpg", "wb").write(
+                response.content
+            )
+            image_clip = ImageClip(self.local_dest + wordts["word"] + ".jpg")
             return image_clip
         except:
             return None
--- samples/basic_example_usage.py
+++ samples/basic_example_usage.py
@@ -5,20 +5,20 @@ from owoi_audio_to_clip.ClipMakerFactory import ClipMakerFactory
 from owoi_audio_to_clip.utils import upload_audio_to_gcs, upload_video_to_gcs, purge_local_storage_images
 
 
-upload_audio_to_gcs("owoi_bucket", "username", "lose_yourself", "../tmp/")
+upload_audio_to_gcs("owoi_bucket", "username", "tell_me_which_one", "../tmp/")
 print("Audio upload done!")
 
-transcript_factory_test = TranscriptFactory("gs://owoi_bucket/username/audios/lose_yourself.wav")
+transcript_factory_test = TranscriptFactory("gs://owoi_bucket/username/audios/tell_me_which_one.wav")
 
 transcript = transcript_factory_test.transcribe_audio_to_text()
 
 print(transcript)
 
-clip_maker_factory = ClipMakerFactory("lose_yourself", "username", transcript, "owoi_bucket", "../tmp/", "lose_yourself")
+clip_maker_factory = ClipMakerFactory("tell_me_which_one", "username", transcript, "owoi_bucket", "../tmp/", "tell_me_which_one")
 
 clip = clip_maker_factory.clip_maker()
 
-upload_video_to_gcs("owoi_bucket", "username", "lose_yourself", "../tmp/")
+# upload_video_to_gcs("owoi_bucket", "username", "tell_me_which_one", "../tmp/")
 print("Video upload done!")
 
 # purge_local_storage_images("../tmp/")
